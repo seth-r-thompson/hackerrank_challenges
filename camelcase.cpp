b8701cd488404d140d610db2e2e07d28bc76a3a3
Seth Thompson <31255198+seth-r-thompson@users.noreply.github.com>
2018-01-23 17:04:57 -0500

    Fixed formatting for the camelcase challenge

--- camelcase.cpp
+++ camelcase.cpp
@@ -1,11 +1,11 @@
 /* View problem: https://www.hackerrank.com/challenges/camelcase/
-/* Applications: Could be modified to parse camelcase strings.
-/*
-/* Returns the number of words in a string that is in camelcase.
-/* 
-/* @param s   A string in camelcase.
-/*
-/* Has an average runtime of O(N) where N is the length of the string passed.
+* Applications: Could be modified to parse camelcase strings.
+*
+* Returns the number of words in a string that is in camelcase.
+* 
+* @param s   A string in camelcase.
+*
+* Has an average runtime of O(N) where N is the length of the string passed.
 */
 
 int camelcase(string s) {
