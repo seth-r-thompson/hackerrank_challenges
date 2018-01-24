de94e827e124faf4e8642edce973e5ae84c7366e
Seth Thompson <31255198+seth-r-thompson@users.noreply.github.com>
2018-01-24 08:51:09 -0500

    Added solution for the time-conversion challenge

---
+++ military_time.cpp
@@ -0,0 +1,15 @@
+/* View problem: https://www.hackerrank.com/challenges/time-conversion/
+* Applications: Converting standard time format to military time.
+*
+* Returns a time in "hh:mm:ss" format.
+* 
+* @param s		A time in "hh:mm:ss:AM" format.
+*
+* Has an average runtime of O(N) where N is the length of the string passed.
+*/
+string to_military_time(string s) {
+	string hr = (stoi(s.substr(0,2)) == 12) ? "00" : s.substr(0,2);
+	if (s[8] == 'P') hr = to_string(stoi(hr) + 12);
+	
+	return hr + s.substr(2,6);
+}
